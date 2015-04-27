98c64c485d0665eb02bd4e6b65d5c142ea50ae3c
Andrew Bovsunovskiy <an.bvsn@gmail.com>
2015-04-27 23:23:29 +0300

    Supervice socket listener.

--- src/qri_app.erl
+++ src/qri_app.erl
@@ -3,22 +3,12 @@
 -export([start/0, start/2, stop/1]).
 
 -define(C_ACCEPTORS, 5000).
--define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
-
+-define(ETS_OPTIONS, [set, public, named_table, {keypos, 1}]).
 -define(ROUTES, [
     {'_', [
         {"/stream", qri_emitter, []}
     ]}
 ]).
-
-
-http_port() ->
-    {ok, Port} = application:get_env(http_port),
-    Port.
-
-socket_port() ->
-    {ok, Port} = application:get_env(socket_port),
-    Port.
 
 start() ->
     application:start(crypto),
@@ -28,42 +18,19 @@ start() ->
     application:start(qri).
 
 start(_StartType, _StartArgs) ->
-    Port = http_port(),
+    {ok, Port} = application:get_env(http_port),
+
     Dispatch = cowboy_router:compile(?ROUTES),
     TransOpts = [{port, Port}],
     ProtoOpts = [{env, [{dispatch, Dispatch}]}],
 
     % Creating ETS table named peers for further mapping: Peer => PID.
-    ets:new(peers, [set, public, named_table, {keypos, 1}]),
+    ets:new(peers, ?ETS_OPTIONS),
 
     cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
     io:format("Starting at port ~p\n", [Port]),
 
-    qri_sup:start_link(),
-
-    listen().
+    qri_sup:start_link().
 
 stop(_State) ->
     ok.
-
-loop(Socket) ->
-    case gen_tcp:recv(Socket, 0) of
-        {ok, Data} ->
-            % Testing data.
-            PIDs = qri_peer:get_pids({<<"123">>, 2286445522}),
-            [PID ! {message, Data} || PID <- PIDs],
-            io:format(">>> Data: ~p ~p\n", [Data, PIDs]),
-            loop(Socket);
-        {error, closed} ->
-            ok
-    end.
-
-accept(LSock) ->
-    {ok, Socket} = gen_tcp:accept(LSock),
-    spawn(fun() -> loop(Socket) end),
-    accept(LSock).
-
-% Listen socket connection and push notifications to the relative PIDs.
-listen() ->
-    {ok, LSock} = gen_tcp:listen(socket_port(), ?TCP_OPTIONS),
-    accept(LSock).
--- src/qri_sup.erl
+++ src/qri_sup.erl
@@ -11,11 +11,38 @@
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
+
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
+loop(Socket) ->
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Data} ->
+            % Testing data.
+            PIDs = qri_peer:get_pids({<<"123">>, 2286445522}),
+            [PID ! {message, Data} || PID <- PIDs],
+            io:format(">>> Data: ~p ~p\n", [Data, PIDs]),
+            loop(Socket);
+
+        {error, closed} ->
+            ok
+    end.
+
+accept(LSock) ->
+    {ok, Socket} = gen_tcp:accept(LSock),
+    spawn(fun() -> loop(Socket) end),
+    accept(LSock).
+
 start_link() ->
+    {ok, SPort} = application:get_env(socket_port),
+
+    % Listen socket connection and push notifications to the relative PIDs.
+    spawn_link(fun() ->
+        {ok, LSock} = gen_tcp:listen(SPort, ?TCP_OPTIONS),
+        accept(LSock)
+    end),
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% ===================================================================
@@ -24,4 +51,3 @@ start_link() ->
 
 init([]) ->
     {ok, { {one_for_one, 2, 5}, []} }.
-

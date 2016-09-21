8c12f49b4e631902146ab6ede7873a3f09d7f7eb
va-dev <vadimanikin1@gmail.com>
2016-09-21 12:11:53 +0300

    Implement: heartbeat messages.

--- src/qri_sup.erl
+++ src/qri_sup.erl
@@ -3,13 +3,27 @@
 -behaviour(supervisor).
 
 -export([start_link/0]).
+-export([ticks_emitter/0]).
 -export([init/1]).
 
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
 -define(ETS_OPTIONS, [set, public, named_table, {keypos, 1}]).
 -define(TCP_OPTIONS, [binary, {packet, asn1}, {active, false}, {reuseaddr, true}]).
+-define(TICK_INTERVAL, 30).
 
+
+send_peers_ticks([]) -> ok;
+send_peers_ticks([PeersPIDs|Rest]) ->
+    [PID ! {message, "Tick"} || PID <- PeersPIDs],
+    send_peers_ticks(Rest).
+
+ticks_emitter() ->
+    % Example: [[PID11, ..., PID1N], ..., [PIDM1, ..., PIDML]],
+    % where PID11, ..., PID1N - PIDs of first peer,
+    % PIDM1, ..., PIDML - another peers PIDs.
+    PIDsByPeers = ets:select(peers, [{{'_','_', '$3'}, [], ['$3']}]),
+    send_peers_ticks(PIDsByPeers).
 
 emitter([Peer, Checksum, Message]) ->
     PIDs = qri_peer:get_pids({Peer, Checksum}),
@@ -48,5 +62,7 @@ init([]) ->
     ets:new(peers, ?ETS_OPTIONS),
 
     listen(),
+
+    timer:apply_interval(timer:seconds(?TICK_INTERVAL), qri_sup, ticks_emitter, []),
 
     {ok, { {one_for_one, 5, 10}, []} }.
